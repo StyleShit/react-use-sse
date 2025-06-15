48b722886c61b58ab0d72e50750c00d69eb67e1e
StyleShit <32631382+StyleShit@users.noreply.github.com>
2025-06-15 20:09:26 +0300

    feat: initial version

---
+++ .changeset/sweet-turkeys-pull.md
@@ -0,0 +1,5 @@
+---
+'react-use-sse': patch
+---
+
+Initial version
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,57 @@
-export default function helloWorld() {
-	return 'Hello World!';
+import { useEffect, useState } from 'react';
+
+type TReturn<TData> =
+	| {
+			status: 'pending';
+			data: null;
+			isPending: true;
+			isSuccess: false;
+			isError: false;
+	  }
+	| {
+			status: 'success';
+			data: TData;
+			isPending: false;
+			isSuccess: true;
+			isError: false;
+	  }
+	| {
+			status: 'error';
+			data: null;
+			isPending: false;
+			isSuccess: false;
+			isError: true;
+	  };
+
+type Status = 'pending' | 'success' | 'error';
+
+export function useSSE<TData = unknown>(url: string): TReturn<TData> {
+	const [data, setData] = useState<TData | null>(null);
+	const [status, setStatus] = useState<Status>('pending');
+
+	useEffect(() => {
+		const eventSource = new EventSource(url);
+
+		eventSource.addEventListener('message', (event) => {
+			setData(event.data as TData);
+			setStatus('success');
+		});
+
+		eventSource.addEventListener('error', () => {
+			setData(null);
+			setStatus('error');
+		});
+
+		return () => {
+			eventSource.close();
+		};
+	}, [url]);
+
+	return {
+		data,
+		status,
+		isPending: status === 'pending',
+		isSuccess: status === 'success',
+		isError: status === 'error',
+	} as TReturn<TData>;
 }
